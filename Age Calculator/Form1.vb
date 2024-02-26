835ef6936194b57d7b772d3cd6f91f0e433d4822
Tshepang McElhenny <105787557+JM0501@users.noreply.github.com>
2024-02-26 12:27:35 +0200

    Add files via upload
    Afun calculator i did back in 2021

---
+++ Age Calculator/Form1.vb
@@ -0,0 +1,17 @@
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+Imports System.Security.AccessControl
+
+Public Class Form1
+
+    Private Age, Year As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Year = CInt(InputBox("Enter your birthyear!", "Prompt Year"))
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Age = (2023 - Year)
+        TextBox1.Text = CStr(Age) & " Years Old"
+    End Sub
+End Class
